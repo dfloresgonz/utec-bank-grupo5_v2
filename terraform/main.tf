369803ba1ee1bc35aa2896d6aba29608704325a7
Diego Rafael Flores Gonzales <diegorflores@bcp.com.pe>
2025-06-23 23:44:39 -0500

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -181,8 +181,8 @@ resource "aws_lambda_function" "mlflow_sagemaker_lambda" {
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
   role          = aws_iam_role.lambda_exec.arn
-  filename      = "src/lambda_function.zip"
-  source_code_hash = filebase64sha256("src/lambda_function.zip")
+  filename      = "../src/lambda_function.zip"
+  source_code_hash = filebase64sha256("../src/lambda_function.zip")
 
   environment {
     variables = {
